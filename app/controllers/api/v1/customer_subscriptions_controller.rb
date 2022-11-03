173ba84438d1e7a0fd7b687446cb29935abe2928
MaryT573 <81441711+MaryT573@users.noreply.github.com>
2022-11-03 13:06:31 -0600

    add update method

--- app/controllers/api/v1/customer_subscriptions_controller.rb
+++ app/controllers/api/v1/customer_subscriptions_controller.rb
@@ -6,7 +6,7 @@ class Api::V1::CustomerSubscriptionsController < ApplicationController
             subscription = Subscription.find_by(title: params[:title])
             new_sub = CustomerSubscription.create(customer: user, subscription: subscription)
             if new_sub.save
-                render status: 201
+                render json: SubscriptionSerializer.new_subscription(new_sub), status: 201
             else
                 render json: { error: 'Already Subscribed' }, status: 400
             end
@@ -15,8 +15,18 @@ class Api::V1::CustomerSubscriptionsController < ApplicationController
         end
     end
 
-    def destroy
-        require 'pry'; binding.pry 
+    def update 
+        if CustomerSubscription.find(params[:id])
+            sub = CustomerSubscription.find(params[:id])  
+            if sub.active == true
+                sub.update(active: false)
+                render status: 201
+            else
+                render json: { error: 'Already Cancelled' }, status: 400
+            end
+        else
+            render json: { error: 'Subscription not found' }, status: 400
+        end
     end
 
 end
--- app/serializers/subscription_serializer.rb
+++ app/serializers/subscription_serializer.rb
@@ -2,7 +2,7 @@ class SubscriptionSerializer
     def self.new_subscription(data)
       {
         "data": {
-          
+          "Subscription_id": data.id
               }
       }
     end
